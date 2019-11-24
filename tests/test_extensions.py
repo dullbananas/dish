37d3557e225edcffba4259e2891642568bbd07b0
Dull Bananas <dull.bananas0@gmail.com>
2019-11-24 10:23:34 -0700

    A

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,8 +1,8 @@
-from dish.ext.ansicolor import handle_color_start_tag
+from dish import ext
 
 
 def test_handle_color_start_tag():
-	assert handle_color_start_tag({
+	assert ext.ansicolor.handle_color_start_tag({
 		'fg': 'blue',
 		'bg': 'red',
 	}) == '\033[34;41m'
