8ac8d826dea34d9eee5378ba0071dad5fd7ae004
Dull Bananas <dull.bananas0@gmail.com>
2019-11-24 17:13:59 -0700

    Add cd command
    This commit was made using dish

--- dish/interpreter.py
+++ dish/interpreter.py
@@ -14,11 +14,17 @@ class Interpreter:
 		# Blank lines
 		elif line.strip() == '':
 			return
+		
 		# Print debug information
 		elif line == 'debug\n':
 			click.echo('Configuration values:')
 			for key, val in self.ctx.obj.config.items():
 				click.echo(f'  {key} = {repr(val)}')
+		# cd
+		elif line.startswith('cd '):
+			dirname = line[3:].strip()
+			os.chdir(os.path.expanduser(dirname))
+		
 		# Comments
 		elif line.strip()[0] == '#':
 			return
